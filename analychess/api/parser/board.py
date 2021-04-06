4a81427c040d7706f3fed3b9e9c7daf70a989546
Luca Laissue <luca.laissue@he-arc.ch>
2021-04-06 10:24:07 +0200

    Add File and Row enum

---
+++ analychess/api/parser/board.py
@@ -0,0 +1,20 @@
+from enum import Enum 
+class File(Enum):
+    FILE_A = 0
+    FILE_B = 1
+    FILE_C = 2
+    FILE_D = 3
+    FILE_E = 4
+    FILE_F = 5
+    FILE_G = 6
+    FILE_H = 7
+
+class Row(Enum):
+    ROW_1 = 0
+    ROW_2 = 1
+    ROW_3 = 2
+    ROW_4 = 3
+    ROW_5 = 4
+    ROW_6 = 5
+    ROW_7 = 6
+    ROW_8 = 7
