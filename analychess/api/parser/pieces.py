4c16ff12dff5eb2c5c1efa051cd46bfe7ed31100
Luca Laissue <luca.laissue@he-arc.ch>
2021-04-06 10:12:23 +0200

    Add PieceTypeEnum

--- analychess/api/parser/pieces.py
+++ analychess/api/parser/pieces.py
@@ -3,3 +3,12 @@ from enum import Enum
 class PieceColor(Enum):
     WHITE = 0
     BLACK = 1
+
+class PieceType(Enum):
+    PAWN = 0
+    KNIGHT = 1
+    BISHOP = 2
+    ROOK = 3
+    QUEEN = 4
+    KING = 5
+
