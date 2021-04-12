a60024191c02f7ef62609d1cdec474a22aa5c770
Edouard Goffinet <edouard.goffinet@he-arc.ch>
2021-04-12 10:30:30 +0200

    Update CORS

--- analychess/analychess/settings.py
+++ analychess/analychess/settings.py
@@ -61,7 +61,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:8000',
   'http://localhost:8080',
-  'https://analychess.srvz-webapp.he-arc.ch/'
+  'https://analychess.srvz-webapp.he-arc.ch'
 )
 
 ROOT_URLCONF = 'analychess.urls'
