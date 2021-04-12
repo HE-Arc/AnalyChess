c7d84b94d1f28d3143d20333617753238e067cb4
Edouard Goffinet <edouard.goffinet@he-arc.ch>
2021-04-12 15:40:11 +0200

    change token lifetine

--- analychess/analychess/settings.py
+++ analychess/analychess/settings.py
@@ -172,8 +172,8 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=10),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(hours=2),
 }
 
 AUTH_USER_MODEL = 'api.MyUser'
