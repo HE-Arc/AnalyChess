d7e5b9dc2f06e85ec0237c0ddfff9ea8dc2e7548
Edouard Goffinet <edouard.goffinet@he-arc.ch>
2021-04-12 12:18:14 +0200

    Adding the parser + JSON serialization of pgn with L.L

--- analychess/api/urls.py
+++ analychess/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('upload_pgn', views.upload_pgn),
     path('user', views.UserList.as_view(), name='user-list'),
     path('user/<int:pk>', views.UserDetail.as_view()),
     path('game', views.GameList.as_view(), name='game-list'),
