06a350d9943cf8e9dfef74e694f7e0337a9ecce9
unknown <marie.beguin@he-arc.ch>
2021-04-16 04:54:10 +0200

    Add test menu

--- analychess_front/src/views/GameViewer.vue
+++ analychess_front/src/views/GameViewer.vue
@@ -1,11 +1,11 @@
 <template>
     <section>
-        <!--<h1>GameViewer</h1>
+        <h1>GameViewer</h1>
         <h2>Test purpose only</h2>
         <router-link to="/">Home</router-link>
         <router-link to="/game">Game</router-link>
         <router-link to="/test">Test</router-link>
-        <router-link to="/logout">Logout</router-link>-->
+        <router-link to="/logout">Logout</router-link>
         <Board :selectedMoveIndex="1" v-bind:game="this.game"/>
     </section>
 </template>
