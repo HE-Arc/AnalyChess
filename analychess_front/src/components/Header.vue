72e30dc5de7e5080b2efef46c5eaccc132b1e974
unknown <marie.beguin@he-arc.ch>
2021-04-16 11:47:26 +0200

    Add link in nav bar

--- analychess_front/src/components/Header.vue
+++ analychess_front/src/components/Header.vue
@@ -1,7 +1,17 @@
 <template>
-<div class="w3-container w3-black">
-  <h1>Analychess</h1>
-</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Analychess</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <router-link to="/" class="nav-link">Home </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/logout" class="nav-link" >Logout</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
 </template>
 
 <script>
