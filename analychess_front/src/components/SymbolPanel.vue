fd8e003a7bb0921e4754fc05eaf9a01b67a707b3
unknown <marie.beguin@he-arc.ch>
2021-04-16 11:15:38 +0200

    Add symbols in SymbolPanel

--- analychess_front/src/components/SymbolPanel.vue
+++ analychess_front/src/components/SymbolPanel.vue
@@ -3,38 +3,38 @@
     <div class="row justify-content-md-center d-inline-flex">
       <div class="col p-0">
         <ul class="list-group">
-          <li class="list-group-item">Good move</li>
-          <li class="list-group-item">Mistake</li>
-          <li class="list-group-item">Brillant move</li>
-          <li class="list-group-item">Blunder</li>
-          <li class="list-group-item">Interestiong move</li>
-          <li class="list-group-item">Dubious move</li>
-          <li class="list-group-item">Only move</li>
-          <li class="list-group-item">Zugzwang</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">!</button>Good move</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">?</button>Mistake</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">!!</button>Brillant move</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">??</button>Blunder</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">!?</button>Interestiong move</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">?!</button>Dubious move</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">□</button>Only move</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">⨀</button>Zugzwang</li>
         </ul>
       </div>
       <div class="col p-0">
         <ul class="list-group">
-          <li class="list-group-item">Equal position</li>
-          <li class="list-group-item">Unclear position</li>
-          <li class="list-group-item">White is slightly better</li>
-          <li class="list-group-item">Black is slightly better</li>
-          <li class="list-group-item">White is better</li>
-          <li class="list-group-item">Black is better</li>
-          <li class="list-group-item">White is winning</li>
-          <li class="list-group-item">Black is winning</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">=</button>Equal position</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">∞</button>Unclear position</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">⩲</button>White is slightly better</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">⩱</button>Black is slightly better</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">±</button>White is better</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">∓</button>Black is better</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">+-</button>White is winning</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">-+</button>Black is winning</li>
         </ul>
       </div>
       <div class="col p-0">
         <ul class="list-group">
-          <li class="list-group-item">Novelty</li>
-          <li class="list-group-item">Development</li>
-          <li class="list-group-item">Initiative</li>
-          <li class="list-group-item">Attack</li>
-          <li class="list-group-item">Counterplay</li>
-          <li class="list-group-item">Time trouble</li>
-          <li class="list-group-item">With compensation</li>
-          <li class="list-group-item">With the idea</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">N</button>Novelty</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">↑↑</button>Development</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">↑</button>Initiative</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">→</button>Attack</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">⇆</button>Counterplay</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">⊕</button>Time trouble</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">=∞</button>With compensation</li>
+          <li class="list-group-item"><button type="button" class="btn btn-sm">∆</button>With the idea</li>
 
         </ul>
       </div>
